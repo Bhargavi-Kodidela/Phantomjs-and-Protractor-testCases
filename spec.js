bb84f369f84ca6da837459d55574760586b5fd5d
Bhargavi-Kodidela <bhargavi@vivasa.in>
2017-01-19 17:29:31 +0530

    Update spec.js

--- spec.js
+++ spec.js
@@ -7,12 +7,5 @@ describe('Mesaforte-ui Demo App', function(){
 	    element(by.model('login.username')).sendKeys('Suport');
 	    element(by.model('login.passwd')).sendKeys('w4Suport');
 	});
-
-	it('Testing a Ruleset', function(){
-		browser.get('http://localhost:8888/#/home/plan/ruleset/');
-
-		expect(browser.getTitle()).toEqual('Mesaforte');
-		element(by.model('beanCtrl.gridOptions.quickFilterText')).sendKeys('dent');
-	});
 });
 
